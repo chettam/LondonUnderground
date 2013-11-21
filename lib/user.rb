6ce9d95d9dbd61457cf8852e2a8cbc80837f0dac
Jean-Baptiste Blanc <jbblanc@gmail.com>
2013-11-21 16:17:57 +0000

    Added Names  and status to users

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,18 @@
 class User
 
+	attr_accessor :name
+
+	def initialize
+		@is_inside_lu = false
+	end
+
+	def is_inside_lu?
+		@is_inside_lu
+	end
+
+	def is_inside_lu
+		@is_inside_lu = true
+	end
+
+
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,25 @@
 require_relative '../lib/user'
 
 describe User  do
-	
+
+	let(:user) {User.new}
+
+	context "should have" do
+		it "have a name" do
+			user.name = "JB"
+			expect(user.name).to eq("JB")
+		end
+	end
+
+	context "should know" do
+		it "if the user currently inside LU" do
+			user.is_inside_lu
+			expect(user.is_inside_lu?).to  be_true
+		end
+	end
+	context "should by default" do
+		it "set the user to be outside " do
+			expect(user.is_inside_lu?).not_to be_true
+		end
+	end
 end
